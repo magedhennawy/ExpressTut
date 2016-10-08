c594826e74a1d945aa48745de053813c3f4c9a14
Maged <magedhennawy@gmail.com>
2016-10-08 15:12:13 -0400

    set up

--- index.js
+++ index.js
@@ -8,4 +8,30 @@ var app = express();
 
 app.disable('x-powered-by');
 
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars',handlebars.engine);
+app.set('view engine', 'handelbars');
+
+// MORE IMPORTS HERE
+
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',function (req, res) {
+   res.render('home');
+});
+
+
+
+
+
+
+
+
+app.listen(app.get('port',function () {
+    console.log("express started on http://localhost:" + app.get('port_'));
+}));
 
